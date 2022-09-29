d50033c83de3147b9b9842c88aad2784251f4406
anhnt15cp <anhnt15cp@gmail.com>
2022-09-29 15:56:26 +0700

    tuananh

--- App_IOS_SWIFT/NetFlix/NetFlix/HomeCreen/LogginViewController.swift
+++ App_IOS_SWIFT/NetFlix/NetFlix/HomeCreen/LogginViewController.swift
@@ -10,14 +10,15 @@ import UIKit
 class LogginViewController: UIViewController {
     @IBOutlet weak var myTextField1: UITextField!
     @IBOutlet weak var myTextField2: UITextField!
+    @IBOutlet weak var myActivity: UIActivityIndicatorView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        DispatchQueue.main.asyncAfter(deadline: .now()+5, qos: .default, flags: .assignCurrentContext) { [self] in
+            self.myActivity.stopAnimating()
+            
+        }
     }
-    
-    
-    
     @IBAction func backButton(_ sender: UIButton) {
         dismiss(animated: true)
     }
@@ -35,19 +36,21 @@ class LogginViewController: UIViewController {
             alertController.addAction(UIAlertAction(title: "OK", style: .default))
             present(alertController, animated: true)
         }else {
-            let movieVC = MovieViewController()
-            movieVC.modalPresentationStyle = .fullScreen
-            present(movieVC, animated: true)
-            
+            myActivity.startAnimating()
+            DispatchQueue.main.asyncAfter(deadline: .now()+5, qos: .default, flags: .assignCurrentContext) {
+                let movieVC = MovieViewController()
+                movieVC.modalPresentationStyle = .fullScreen
+                self.present(movieVC, animated: true)
+            }
         }
-        
-        
     }
+    
     @IBAction func getPassword(_ sender: Any) {
         let getPassword = GetPassword2ViewController()
         getPassword.modalPresentationStyle = .fullScreen
         present(getPassword, animated: true)
     }
+    
     @IBAction func showPassword(_ sender: Any) {
         myTextField2.isSecureTextEntry.toggle()
     }
